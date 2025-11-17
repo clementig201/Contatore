38af17b6815d173bac2cf130bc98f3f47b6d9441
clementig201 <clementig201@gmail.com>
2025-11-17 12:25:42 +0100

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,39 @@
-public static void main(String[] args) {
-    Contatore counter = new Contatore();
+public class Main {
+    public static void main(String[] args) {
+        Contatore counter = new Contatore();
 
-    // Create multiple threads to increment the counter
-    Thread t1 = new Thread(() -> counter.increment());
-    Thread t2 = new Thread(() -> counter.increment());
+        // Create multiple threads to increment the counter
+        Thread t1 = new Thread(() -> counter.increment());
+        Thread t2 = new Thread(() -> counter.increment());
 
-    t1.start();
-    t2.start();
+        t1.start();
+        t2.start();
 
-    try {
-        t1.join();
-        t2.join();
-    } catch (InterruptedException e) {
-        e.printStackTrace();
-    }
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
-    System.out.println("Final count: " + counter.getCount());
-    ContatoreSync example = new ContatoreSync();
+        System.out.println("Final count: " + counter.getCount());
+        ContatoreSync example = new ContatoreSync();
 
 // Create multiple threads to increment the counter
-    Thread t3 = new Thread(() -> example.increment());
-    Thread t4 = new Thread(() -> example.increment());
+        Thread t3 = new Thread(() -> example.increment());
+        Thread t4 = new Thread(() -> example.increment());
 
-    t3.start();
-    t4.start();
+        t3.start();
+        t4.start();
 
-    try {
-        t3.join();
-        t4.join();
-    } catch (InterruptedException e) {
-        e.printStackTrace();
+        try {
+            t3.join();
+            t4.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Final count: " + example.getCount());
+
     }
-
-    System.out.println("Final count: " + example.getCount());
-
 }
-
